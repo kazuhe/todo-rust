798749749c9a7c67c0a722a6beb6fca2c6e01ecf
kazuhe <omc.ohara@gmail.com>
2024-11-18 01:17:43 +0900

    入力された title をもとに ToDo Item を作成し、command をもとにアクションを切り替えられるように

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,19 @@
+mod processes;
 mod state;
 mod todo;
 
-use serde_json::json;
-use state::{read_file, write_to_file};
 use std::env;
 use todo::enums::TaskStatus;
-use todo::traits::create::Create;
-use todo::traits::delete::Delete;
-use todo::traits::edit::Edit;
-use todo::traits::get::Get;
-use todo::{todo_factory, ItemTypes};
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // let status = &args[1];
-    // let title = &args[2];
-    let mut state = read_file("./state.json");
-    println!("変更前の operation: {:?}", state);
-
-    // state.insert(title.to_string(), json!(status));
-    // println!("変更後の operation: {:?}", state);
-    // write_to_file("./state.json", &mut state);
-
-    let todo_item = todo_factory("ゴミ捨て", TaskStatus::Pending);
-
-    match todo_item {
-        ItemTypes::Done(item) => {
-            item.get(&item.super_struct.title, &state);
-            item.delete(&item.super_struct.title, &mut state);
-        }
-        ItemTypes::Pending(item) => {
-            item.create(
-                &item.super_struct.title,
-                &item.super_struct.status.stringify(),
-                &mut state,
-            );
-            item.get(&item.super_struct.title, &state);
-            // item.set_to_done(&item.super_struct.title, &mut state);
-        }
-    }
-
-    let todo_item = todo_factory("洗濯", TaskStatus::Pending);
-
-    match todo_item {
-        ItemTypes::Done(item) => {
-            item.get(&item.super_struct.title, &state);
-            item.delete(&item.super_struct.title, &mut state);
-        }
-        ItemTypes::Pending(item) => {
-            item.get(&item.super_struct.title, &state);
-            item.set_to_done(&item.super_struct.title, &mut state);
-        }
-    }
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+    let title = &args[2];
+    let state = state::read_file("./state.json");
+    let status = match &state.get(title) {
+        Some(result) => result.to_string().replace('\"', ""),
+        None => "Pending".to_owned(),
+    };
+    let item = todo::todo_factory(title, TaskStatus::from_string(status));
+    processes::process_input(item, command.to_string(), &state);
 }
--- src/todo/enums.rs
+++ src/todo/enums.rs
@@ -10,4 +10,12 @@ impl TaskStatus {
             Self::Pending => "Pending".to_string(),
         }
     }
+
+    pub fn from_string(input_string: String) -> Self {
+        match input_string.as_str() {
+            "Done" => Self::Done,
+            "Pending" => Self::Pending,
+            _ => panic!("入力された {} はサポートしていません", input_string),
+        }
+    }
 }
